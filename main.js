774125b4f4572332b312e700777dacfebaff6eaa
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-02 20:34:44 +0200

    80% to run application small problems

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
-let options = [];
+let options = ['option 1', 'option 2'];
 
 
 
-//add elements
-const addOptions = (e) => {
-    e.preventDefault();
-    document.querySelector('input').value;
+const showAdvice = () => {
+    const index = Math.floor(Math.random() * options.length);
+    document.querySelector('h1').textContent = options[index];
 }
 
-document.querySelector(".add").addEventListener("click", addOptions)
+
+const addOption = (e) => {
+
+    e.preventDefault();
+    const input = document.querySelector('input');
+    input.value = "";
+    alert(`add ${input.value}`);
+    options.push(input.value);
+}
 
 
-
-
-//delete elements
 const deleteElements = (e) => {
+
     e.preventDefault();
     options.length = 0;
-    
+    document.querySelector('h1').textContent = "";
+
     }
+
+
+    const showOption = () => {
+    alert(options.join(" -- --"));
+    }
+
+//show elements
+document.querySelector(".add").addEventListener("click", addOption)
+
+
 //delete elements
 document.querySelector(".clean").addEventListener("click", deleteElements)
 
 
-
 //show elements
-const showOption = () => {
-    alert(options.join(" --- ---"));
-}
+document.querySelector(".ShowOptions").addEventListener("click", showOption)
 
-document.querySelector(".ShowOption").addEventListener("click", showOptions)
+
+document.querySelector(".ShowAdvice").addEventListener("click", showAdvice)
 
