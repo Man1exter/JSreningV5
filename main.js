a0396e49b407a50de04a2a1f70efc6c20cb3a863
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-02 20:08:54 +0200

    commit for results of funtions

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 let options = [];
 
-const btnAdd = document.querySelector(".add").addEventListener("click", addOptions)
+
+
+
+
+//add elements
+document.querySelector(".add").addEventListener("click", addOptions)
 
 const addOptions = () => {
-  
+   let option = options.push();
+}
+
+
+
+//delete elements
+document.querySelector(".clean").addEventListener("click", deleteElements)
+
+const deleteElements = (e) => {
+e.preventDefault();
+options.length = 0;
+
+}
+
+
+
+//show elements
+document.querySelector(".ShowOptions").addEventListener("click", showOptions)
+
+const showOptions = () => {
+    alert(options.join(" --- ---"));
 }
