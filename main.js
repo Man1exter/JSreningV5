8784beccdbc8e9d4b46672aac2d23f73fad16b18
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-09 10:51:37 +0200

    little correct eff

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ document.querySelector(".ShowAdvice").addEventListener("click", showAdvice);
 
 const spanText = document.querySelector('.text')
 const spanTyp = document.querySelector('.cursor')
-const texet = 'Hey Hey welcome sia rc well good nice spark sit the items dol webistes complete functions'
+const texet = 'Hey Hey welcome sia rc well good nice spark sit the items dol webistes complete functions';
 
 
 let moveLetter = 0;
